396f6d49522085f1b8ed78462f6c3ac86868f33d
Guilherme Goll <guilhermecgoll@gmail.com>
2020-03-17 12:43:13 -0300

    primeira parte

--- redis/README.md
+++ redis/README.md
@@ -1,1 +1,30 @@
-# redis
+# Redis
+
+Este projeto implementa um bingo, utilizando Python, como linguagem de programação, e o Redis, como banco de dados.
+
+## Pré-requisitos
+
+- Ter o python 3.7 ou superior instalado.
+- Ter um servidor local do Redis em execução.
+
+## 1 - Setup e configuração
+
+### 1.1 Clonar o repositório
+
+Clone o repositório em questão:
+
+``git clone https://github.com/guilhermecgoll/pos-bancosnosql.git``
+
+### 1.2 Baixar as dependências
+
+Obs: os comandos abaixo deverão ser executados através da linha de comando.
+
+- Navegue até o diretório ``redis``.
+- Em seguida crie um diretório virtual, ative-o, e instale a dependência do ``redis``:
+
+```sh
+python -m venv .venv
+.\.venv\Scripts\activate
+pip install redis
+```
+
--- redis/redinsgo.py
+++ redis/redinsgo.py
@@ -1,24 +1,44 @@
+import random
+import json
+
 import redis
 
+colecaoUsuarios = "usuarios"
+colecaoCartelas = "cartelas"
+colecaoNumeros = "numerosSorteio"
 
-def gerar_cartelas():
-    print('gerar as cartelas')
+def gerar_dados():
+    for i in numerosParaSorteio:
+        r.sadd(colecaoNumeros, i)
 
-def gerar_usuarios():
-    print('gerar usuarios')
+    for i in usuarios:
+        _nomeCartela = "cartela" + str(i)
+        _numerosCartela = random.sample(numerosParaSorteio, 15)
+        _cartela = { "nome" : _nomeCartela,
+                     "numeros" : _numerosCartela,
+                     "pontos" : 0 }
+
+        _nomeUsuario = "usuario" + str(i)
+        _usuario = { "nome" : _nomeUsuario,
+                     "cartela" : _cartela }
+        r.sadd(colecaoUsuarios, json.dumps(_usuario))
+        r.sadd(colecaoCartelas, json.dumps(_cartela))
 
 def iniciar_bingo():
-    gerar_cartelas()
-    gerar_usuarios()
-    #for...
-    print('sorteando numeros')
+    gerar_dados()
     calcular_score()
 
 def calcular_score():
-    print('score')
+    ganhador = False
+    while ganhador is False:
+        sorteado = r.spop(colecaoNumeros, 1)
+        for i in cart
+
 
 if __name__ == '__main__':
-    # r = redis.Redis()
-    # r.mset({"Croatia": "Zagreb", "Bahamas" : "Nassau"})
-    # print(r.get("Bahamas"))
+    r = redis.Redis()
+    qtdJogadores = 50
+    numerosParaSorteio = range(1, 101)
+    usuarios = range(1, 51)
+    cartelas = []
     iniciar_bingo()
